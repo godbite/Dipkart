adbb0008597b2ab8daadaa1e12a02784a4aba9f5
godbite <devanshvashist2004@gmail.com>
2026-01-08 09:28:01 +0530

    feat(backend): add cart API routes

---
+++ backend/src/routes/cartRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getCart,
+  addToCart,
+  updateCartItem,
+  removeFromCart,
+  clearCart
+} = require('../controllers/cartController');
+
+router.get('/', getCart);
+router.post('/add', addToCart);
+router.put('/update', updateCartItem);
+router.delete('/remove/:productId', removeFromCart);
+router.delete('/clear', clearCart);
+
+module.exports = router;
